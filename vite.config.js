8e7b0926ec2764d400de512d3191a199f0a37542
Ugo Ricciardi <ugo.ricciardi@vexuvo.com>
2026-07-13 15:58:40 +0200

    perf(build): split React Router and Supabase vendors

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,32 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+
+  build: {
+    rolldownOptions: {
+      output: {
+        codeSplitting: {
+          groups: [
+            {
+              name: "vendor-react",
+              test: /node_modules[\\/](react|react-dom|scheduler)[\\/]/,
+              priority: 30,
+            },
+            {
+              name: "vendor-router",
+              test: /node_modules[\\/](react-router|react-router-dom)[\\/]/,
+              priority: 25,
+            },
+            {
+              name: "vendor-supabase",
+              test: /node_modules[\\/]@supabase[\\/]/,
+              priority: 20,
+            },
+          ],
+        },
+      },
+    },
+  },
+});
